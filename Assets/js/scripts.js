f7a9aa12c3ba9eb0067f6520d7c67bdf0bd50478
ezc0031 <135243593+ezc0031@users.noreply.github.com>
2024-07-17 16:33:58 -0500

    Added javascript for burger menu

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -20,6 +20,15 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  //Nav hamburgerburger selections
+  const burger = document.querySelector("#burger-menu");
+  const ul = document.querySelector("nav ul");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", () =>{
+    ul.classList.toggle("show")
+  });
   
 
 
